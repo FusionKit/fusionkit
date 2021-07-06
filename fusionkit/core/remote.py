39cb7bfa1bdc9e147623068807818edc365bd8f2
Garud Snoep <gsnoep42@gmail.com>
2021-07-06 17:46:43 +0200

    Added copy_file_to_remote() and byte decoding of the remote response

--- fusionkit/core/remote.py
+++ fusionkit/core/remote.py
@@ -1,4 +1,5 @@
-import subprocess, sys
+import subprocess 
+import sys, os
 
 class Remote:
     '''
@@ -8,6 +9,8 @@ class Remote:
         self.response = []
 
     def send_commands(self, bash='ssh',host=None,commands=None,verbose=True):
+        # Ports are handled in ~/.ssh/config since we use OpenSSH
+        print('Executing remote commands over {} on {}...'.format(bash,host))
         remote = subprocess.Popen([bash, host, commands],
                             shell=False,
                             stdout=subprocess.PIPE,
@@ -16,9 +19,17 @@ class Remote:
         self.response = [line.decode("utf-8") for line in self.response]
         if self.response == []:
             error = remote.stderr.readlines()
+            error = [line.decode("utf-8") for line in error]
             print("ERROR: {}".format(error), file=sys.stderr)
         else:
             if verbose:
                 print(''.join(self.response))
             return self.response
     
+    def copy_file_to_remote(origin=None,destination=None):
+        try:
+            print('Moving file to: {}'.format(destination))
+            process = subprocess.Popen(['scp {} {}'.format(origin,destination)],shell=True)
+            sts = os.waitpid(process.pid, 0)
+        except CalledProcessError:
+            print('ERROR: Connection to host failed!')
